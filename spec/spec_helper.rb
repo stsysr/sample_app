f3cc25d88510305460c243faf109fca9b9412b33
Satoshi YASHIRO <s-yashiro@mapion.co.jp>
2014-11-27 10:57:49 +0900

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,10 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require 'rspec/rails'
+require 'capybara'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -37,6 +41,8 @@ RSpec.configure do |config|
     # `true` in RSpec 4.
     mocks.verify_partial_doubles = true
   end
+
+  config.include Capybara::DSL
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
